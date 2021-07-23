1f1fdbadc9c4f37ebe5132ce6bcd880aedd8028b
victorequena22 <victorequena22@gmail.com>
2021-07-23 18:10:56 -0400

    relativo de la imagen

--- api/Reportes/Encabezado.php
+++ api/Reportes/Encabezado.php
@@ -1,7 +1,7 @@
 <page_header>
     <div class="col-12">
         <div style="width: 50px; height: 50px;top: 5px;left: 10px;position: absolute">
-            <img style="width: 100%; height: 100%;" src="../public/imagenes/<?php echo $company['logo']; ?>" alt="Logo"> 
+            <img style="width: 100%; height: 100%;" src="<?php echo DR ?>/../public/imagenes/<?php echo $company['logo']; ?>" alt="Logo"> 
         </div>
         <div class="col-11 col-offset-1" style="font-size:8px;">
             <span class="text-encabezado" style="font-size:16px;"><?php echo $company['nombre'] ?></span><br>
