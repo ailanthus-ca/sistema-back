2591514e16a98bf912d20fcd83f9c565ec8cd0e3
victor <victorequena22@gamil.com>
2021-08-16 13:13:04 -0400

    git updater

--- updater.sh
+++ updater.sh
@@ -1,3 +1,3 @@
 cd /home/mecicama/public_html/sisapi-v2/
 git reset --hard
-git pull https://victorequena22:#Sandra22@github.com/victorequena22/sisapi-v2.git main
+git pull https://victorequena22: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@github.com/victorequena22/sisapi-v2.git main
