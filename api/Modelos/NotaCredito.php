a403f51cc3aec97ded8560ba9f3f4bcfa6ee5ccb
victor <victorequena22@gamil.com>
2021-09-16 15:27:55 -0400

    notas de credito y debito

---
+++ api/Modelos/NotaCredito.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Modelos;
+
+class NotaCredito extends \conexion {
+
+    function lista() {
+        
+    }
+
+    function nuevo() {
+        
+    }
+
+    function detalles() {
+        
+    }
+
+    function cancselar() {
+        
+    }
+
+    function finalizar() {
+        
+    }
+
+}
